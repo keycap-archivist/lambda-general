63fe56a08b7361da9f667447607d7f013a7cc762
Vincent LE GOFF <vince.legoff@gmail.com>
2021-06-08 10:16:55 +0200

    fix

--- src/lambda.ts
+++ src/lambda.ts
@@ -3,7 +3,7 @@ import app from './app';
 
 const proxy = awsLambdaFastify(app);
 const f = function (event, context, callback) {
-  if (event.cookies.length) {
+  if (event.cookies && event.cookies.length) {
     event.headers['cookie'] = event.cookies.join(';');
   }
   return proxy(event, context, callback);
